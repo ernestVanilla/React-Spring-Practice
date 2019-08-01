4a20b1960ef95a9cedd413869614467a3bea1bac
Ernest <ernestv748@hotmail.com>
2019-07-31 22:49:18 -0400

    Followed tutorial for react spring

---
+++ src/components/Component3.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export default function Component3() {
+  return (
+    <div style={c3Style}>
+      <h1>Component 3</h1>
+      <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sed, quam placeat! Veritatis ipsum voluptatem voluptate esse nam assumenda maxime tempora laboriosam beatae, quae iure porro consequuntur rem? Velit, voluptatum libero?</p>
+    </div>
+  )
+}
+
+const c3Style = {
+  background: 'skyblue',
+  color: 'white',
+  padding: '1.5rem 1.5rem 5rem 1.5rem'
+};
